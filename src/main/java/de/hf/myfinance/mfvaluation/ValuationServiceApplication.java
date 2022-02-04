257255cbadc18ea537f22294c9ab13ccec8a3724
holger <holger@hfischer.org>
2022-02-04 13:14:27 +0100

    add open api

--- src/main/java/de/hf/myfinance/mfvaluation/ValuationServiceApplication.java
+++ src/main/java/de/hf/myfinance/mfvaluation/ValuationServiceApplication.java
@@ -19,14 +19,6 @@ public class ValuationServiceApplication {
 	@Value("${api.common.version}")         String apiVersion;
 	@Value("${api.common.title}")           String apiTitle;
 	@Value("${api.common.description}")     String apiDescription;
-	@Value("${api.common.termsOfService}")  String apiTermsOfService;
-	@Value("${api.common.license}")         String apiLicense;
-	@Value("${api.common.licenseUrl}")      String apiLicenseUrl;
-	@Value("${api.common.externalDocDesc}") String apiExternalDocDesc;
-	@Value("${api.common.externalDocUrl}")  String apiExternalDocUrl;
-	@Value("${api.common.contact.name}")    String apiContactName;
-	@Value("${api.common.contact.url}")     String apiContactUrl;
-	@Value("${api.common.contact.email}")   String apiContactEmail;
 
 	@Bean
 	RestTemplate restTemplate() {
@@ -38,18 +30,7 @@ public class ValuationServiceApplication {
 		return new OpenAPI()
 				.info(new Info().title(apiTitle)
 						.description(apiDescription)
-						.version(apiVersion)
-						.contact(new Contact()
-								.name(apiContactName)
-								.url(apiContactUrl)
-								.email(apiContactEmail))
-						.termsOfService(apiTermsOfService)
-						.license(new License()
-								.name(apiLicense)
-								.url(apiLicenseUrl)))
-				.externalDocs(new ExternalDocumentation()
-						.description(apiExternalDocDesc)
-						.url(apiExternalDocUrl));
+						.version(apiVersion));
 	}
 
 	public static void main(String[] args) {
